aef86a12716cfd5df0b02ca3bf2279454bd502b6
po1-jfd <jeff.dion@gmail.com>
2026-09-01 19:39:28 +0000

    Update from https://github.com/jfdion/notes-de-cours/commit/ea8f6a9a039d2ed33302d96390d39788e7bd17ca

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.BZb_6k1d.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.C_SLbpLd.js";import{b as a}from"../app.C2P3H5oy.js";import"./chunk-5VM5RSS4.BPu1YSSM.js";import"./chunk-XXDRQBXY.KwVQDUif.js";import"./chunk-KBJHAD2P.BgXTcSfJ.js";import"./chunk-2GRJ4B5K.CTTHB4-Z.js";import"./channel.GoLNUxaS.js";import"./framework.7Akk-szY.js";import"./theme.B-k9V5Ks.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ assets/chunks/virtual_mermaid-config.B9oicH2Z.js
@@ -0,0 +1,1 @@
+const e={securityLevel:"loose",startOnLoad:!1,htmlLabels:!1};export{e as default};
